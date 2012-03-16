04559780899493ce17644edda7c03100ef05958b
Daniel Schien <dschien@gmail.com>
2012-03-16 21:05:20 +0000

    changed paths to server layout

--- openday/settings.py
+++ openday/settings.py
@@ -45,7 +45,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/schien/workspaces/python/openday/media/'
+MEDIA_ROOT = '/var/www/django/openday/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'openday.urls'
 
 TEMPLATE_DIRS = (
-    '/home/schien/workspaces/python/openday/templates'
+    '/var/www/django/openday/templates'
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
