872f8e3841cfc2cff3620565a1e48d381616632a
Daniel Schien <dschien@gmail.com>
2012-03-16 19:29:38 +0000

    new

---
+++ openday/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+     url(r'^$', 'openday.views.start'),
+     url(r'^start', 'openday.views.start'),
+#     url(r'^gender', 'openday.quest.views.gender'),
+     url(r'^gender', 'openday.views.gender2'),
+     url(r'^climate', 'openday.views.climate'),
+     url(r'^app', redirect_to, {'url': 'http://sympact.cs.bris.ac.uk/openday/godee/'}),
+     url(r'^about', 'django.views.generic.simple.direct_to_template', {'template': 'about.html'}),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^contact', 'openday.views.contact'),
+    # Uncomment the next line to enable the admin:
+     url(r'^admin/', include(admin.site.urls)),
+     #(r'^favicon\.ico$', redirect_to, {'url': '/media/favicon.ico'}),
+     (r'^robots\.txt$', 'django.views.generic.simple.direct_to_template', {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+     (r'^favicon\.ico$', redirect_to, {'url': '/media/favicon.ico'}),
+)
---
+++ openday/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
+from forms import ContactForm
+from models import Contact
+
+# Create your views here.
+
+def start(request):    
+    return render_to_response('start.html', context_instance=RequestContext(request))
+
+def climate(request):
+    # no data to show
+    if not request.session :
+        # if it doesn't have a session -> start again
+        return render_to_response('start.html', {'error_message': "Your session had time out. Start again.", }, context_instance=RequestContext(request))
+    # save the information from the form into the session
+    #request.session
+    return render_to_response('climate.html', {}, context_instance=RequestContext(request))
+    
+def gender(request):
+    # no data to show    
+    if not request.session :
+        # if it doesn't have a session -> start again
+        return render_to_response('start.html', {'error_message': "Your session had time out. Start again.", }, context_instance=RequestContext(request))    
+    return render_to_response('gender.html', {}, context_instance=RequestContext(request))
+    
+def gender2(request):
+    # no data to show    
+    if not request.session :
+        # if it doesn't have a session -> start again
+        return render_to_response('start.html', {'error_message': "Your session had time out. Start again.", }, context_instance=RequestContext(request))    
+    return render_to_response('gender.html', {}, context_instance=RequestContext(request))
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+#            topic = form.clean_data['topic']
+#            message = form.clean_data['message']
+#            sender = form.clean_data.get('sender', 'noreply@example.com')
+            topic = form.data['topic']
+            message = form.data['message']
+            sender = form.data.get('sender', 'noreply@example.com')
+            c = Contact(topic=topic, message=message, sender=sender)
+            c.save()
+            return HttpResponseRedirect('/contact/thanks/')
+    else:
+        form = ContactForm()
+    return render_to_response('form.html', {'form': form}, context_instance=RequestContext(request))
