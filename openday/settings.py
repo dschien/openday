97797b870350dbbd10290cea7a87a4d7de1adbb2
Daniel Schien <dschien@gmail.com>
2012-03-18 13:19:24 +0000

    removed pw

--- openday/settings.py
+++ openday/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'opendaydb',                      # Or path to database file if using sqlite3.
         'USER': 'openday',                      # Not used with sqlite3.
-        'PASSWORD': 'PhWfu3UcLX8b',                  # Not used with sqlite3.
+        'PASSWORD': 'xxx',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
