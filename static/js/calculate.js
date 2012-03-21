d1b11ff823cbf50db9b45aacd9e929dae90b4d09
Jorge Zapico <zapico@kth.se>
2012-03-21 13:55:28 +0100

    Adding details

--- static/js/calculate.js
+++ static/js/calculate.js
@@ -1,85 +1,117 @@
-	// Load the Visualization API and the piechart package.
-	google.load('visualization', '1.0', {
-		'packages' : ['corechart']
-	});
+// Load the Visualization API and the piechart package.
+google.load('visualization', '1.0', {
+	'packages' : ['corechart']
+});
 
-	// Set a callback to run when the Google Visualization API is loaded.
-	// google.setOnLoadCallback(drawChart);
+// Set a callback to run when the Google Visualization API is loaded.
+// google.setOnLoadCallback(drawChart);
 
-	// Callback that creates and populates a data table,
-	// instantiates the pie chart, passes in the data and
-	// draws it.
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
 
-	function drawChart(e_serv, e_network, e_acc_net, e_user) {
+function drawChart(e_serv, e_network, e_acc_net, e_user) {
 
-		// Create the data table.
-		var data = new google.visualization.DataTable();
-		data.addColumn('string', 'Component');
-		data.addColumn('number', 'Energy');
-		data.addRows([['Server', e_serv], ['Network', e_network], ['Access Network', e_acc_net], ['User Device', e_user]]);
+	// Create the data table.
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Component');
+	data.addColumn('number', 'Energy');
+	data.addRows([['Server', e_serv], ['Network', e_network], ['Access Network', e_acc_net], ['User Device', e_user]]);
 
-		// Set chart options
-		var options = {
-			'title' : 'Energy of Components',
-			'width' : 400,
-			'height' : 300
-		};
+	// Set chart options
+	var options = {
+		'title' : 'Energy of Components',
+		'width' : 400,
+		'height' : 300
+	};
 
-		// Instantiate and draw our chart, passing in some options.
-		var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-		chart.draw(data, options);
+	// Instantiate and draw our chart, passing in some options.
+	var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
+}
+
+function saveSelectionToSession() {
+
+}
+
+function calc() {
+
+	// duration
+	var durationMins = document.getElementById('duration')
+	var durationSecs = 60
+
+	// power user device
+	if(durationMins.value != "")
+		durationSecs = durationMins.value * 60
+	var deviceType = document.getElementById('device_selected').value
+	String.prototype.trim = function() {
+		return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+	};
+	deviceType = deviceType.trim()
+
+	var p_device = 0
+	switch(deviceType) {
+		case 'phone':
+			p_device = 1
+			break;
+		case 'tablet':
+			p_device = 3
+			break;
+		case 'pc':
+			p_device = 58
+			break;
+		default:
+			// default is laptop
+			p_device = 28
 	}
 
-	function calc() {
+	// service type
+	var serviceType = document.getElementById('service_selected').value
+	serviceType = serviceType.trim()
+	// default for text
+	var dataVolume = 1800000
 
-		var textField = document.getElementById('amount')
-		var duration = 60
-		if(textField.value != "")
-			duration = textField.value*60
-		var deviceType = document.getElementById('device_selected').value
-		var p_device = 0
-		switch(deviceType) {
-			case 'phone':
-				p_device = 1
-				break;
-			case 'tablet':
-				p_device = 3
-				break;
-			case 'laptop':
-				p_device = 28
-				break;
-			case 'pc':
-				p_device = 58
-				break;
-			default:
-				// default is laptop
-				p_device = 28
-		}
-
-		var connectionType = document.getElementById('connection_selected').value
-		var e_acc_net = 0
-		switch(connectionType) {
-			case 'dsl':
-				e_acc_net = 5 * duration
-				break;
-			case 'mobile':
-				e_acc_net = 70
-				break;
-			default:
-				// default is dsl
-				e_acc_net = 5 * duration
-		}
-
-		var e_serv = 104
-		var e_network = 6
-		var e_user = p_device * duration
-		var e_total = e_serv + e_network + e_acc_net + e_user
-		// convert to watthour
-		e_total *= 0.000277777778
-		e_total = Math.round(e_total*100)/100
-		document.getElementById('bignr').innerHTML = "<h1><big>"+ e_total +"</big> wh</h1>"
-		document.getElementById('bignr').innerHTML = "<h1><big>"+ e_total +"</big> wh</h1>"
-		drawChart(e_serv, e_network, e_acc_net, e_user)
-		// alert('total energy :' + e_total)
-
+	if(serviceType == 'video') {
+		// average for video
+		dataVolume = 57492000
 	}
+
+	// power access network
+	var connectionType = document.getElementById('connection_selected').value
+	connectionType = connectionType.trim()
+	var e_acc_net = 0
+	switch(connectionType) {
+		case 'mobile':
+			e_acc_net = dataVolume * 1.8144144197598379e-05
+			break;
+		default:
+			// default is dsl
+			e_acc_net = 10 * durationSecs
+	}
+	var e_origin = 102
+	var e_3rdp = 5.3490346705157022e-06 * dataVolume
+	var e_serv = e_3rdp + e_origin
+	var e_network = 5.8616855E-6 * dataVolume
+	var e_user = p_device * durationSecs
+	var e_total = e_serv + e_network + e_acc_net + e_user
+	// convert to watthour
+	e_total /= 3600
+	e_total = Math.round(e_total * 100) / 100
+	// update the page to show the results
+	document.getElementById('bignr').innerHTML = "<h1><big>" + e_total + "</big> wh</h1>"
+	document.getElementById('details').innerHTML = "<h1>Device: "+ deviceType + ":" + e_serv/3600 + " wh</h1>"
+	drawChart(e_serv, e_network, e_acc_net, e_user)
+	calcLightBulbsAndCarbon(e_total, duration)
+	// save session
+	saveSelectionToSession()
+}
+
+function calcLightBulbsAndCarbon(e_total, duration) {
+	// 	to kWh
+	e_kWh = e_total / 1000
+	carbon = .53 * e_kWh
+	$("div#carbon").text(carbon);
+	lightBulb = 11 * duration
+	percentLightBulb = e_total / lightBulb / 100
+	$("div#lightBulb").text(percentLightBulb);
+}
