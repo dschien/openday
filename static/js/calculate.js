fb45a7e80dfd970d19d5fddb653c672ababfdb4f
Jorge Zapico <zapico@kth.se>
2012-03-22 13:41:50 +0100

    Circle bigger

--- static/js/calculate.js
+++ static/js/calculate.js
@@ -127,7 +127,7 @@ function calc() {
 	var p_serv = (e_serv * 100) / e_total_joule
 	var p_acc_net = (e_acc_net * 100) / e_total_joule
 	// create circles
-	var max_size=6000
+	var max_size=8000
 	var circle_user= Math.round(Math.sqrt(max_size*(e_user/3600)/3.1416))
 	margin = (100-circle_user)/2
 	margintop = (100-circle_user)/3
@@ -137,19 +137,19 @@ function calc() {
 	var circle_network= Math.round(Math.sqrt(max_size*(e_network/3600)/3.1416))
 	margin = (100-circle_network)/2
 	margintop = (100-circle_network)/3
-	document.getElementById('circle_infra').innerHTML = '<div style="width:'+circle_network+'px; height:'+circle_network+'px; margin-left:'+margin+'px;margin-top:'+margintop+'px; background:#51A1B7; -moz-border-radius: 40px; -webkit-border-radius:40px;"></div>'
+	document.getElementById('circle_infra').innerHTML = '<div style="width:'+circle_network+'px; height:'+circle_network+'px; margin-left:'+margin+'px;margin-top:'+margintop+'px; background:#51A1B7; -moz-border-radius: 80px; -webkit-border-radius:80px;"></div>'
 	document.getElementById('text_infra').innerHTML = "<h2>Internet infrastructure</h2><p>" + Math.round(e_network / 36) / 100 + " Wh</p>"
 	
 	var circle_serv= Math.round(Math.sqrt(max_size*(e_serv/3600)/3.1416))
 	margin = (100-circle_serv)/2
 	margintop = (100-circle_serv)/3
-	document.getElementById('circle_server').innerHTML = '<div style="width:'+circle_serv+'px; height:'+circle_serv+'px;margin-left:'+margin+'px;margin-top:'+margintop+'px; background:#4998CE; -moz-border-radius: 40px; -webkit-border-radius:40px;"></div>'
+	document.getElementById('circle_server').innerHTML = '<div style="width:'+circle_serv+'px; height:'+circle_serv+'px;margin-left:'+margin+'px;margin-top:'+margintop+'px; background:#4998CE; -moz-border-radius: 80px; -webkit-border-radius:80px;"></div>'
 	document.getElementById('text_server').innerHTML = "<h2>Servers</h2><p>" + Math.round(e_serv / 36) / 100 + " Wh</p>"
 	
 	var circle_acc_net= Math.round(Math.sqrt(max_size*(e_acc_net/3600)/3.1416))
 	margin = (100-circle_acc_net)/2
 	margintop = (100-circle_acc_net)/3
-	document.getElementById('circle_access').innerHTML = '<div style="width:'+circle_acc_net+'px; height:'+circle_acc_net+'px;margin-left:'+margin+'px;margin-top:'+margintop+'px; background:#A3C3D8; -moz-border-radius: 40px; -webkit-border-radius:40px;"></div>'
+	document.getElementById('circle_access').innerHTML = '<div style="width:'+circle_acc_net+'px; height:'+circle_acc_net+'px;margin-left:'+margin+'px;margin-top:'+margintop+'px; background:#A3C3D8; -moz-border-radius: 40px; -webkit-border-radius:80px;"></div>'
 	document.getElementById('text_access').innerHTML =  "<h2>" + connectionType + " connection</h2><p>" + Math.round(e_acc_net / 36) / 100 + " Wh</p>"
 
 	//drawChart(e_serv, e_network, e_acc_net, e_user)
