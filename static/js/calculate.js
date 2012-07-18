a16737d0ba4a54f6d9306fcf2e523e10b0c05914
Daniel Schien <dschien@gmail.com>
2012-07-18 18:27:43 +0100

    updated

--- static/js/calculate.js
+++ static/js/calculate.js
@@ -53,8 +53,14 @@ function addSelectionToSession() {
 	selectionsArr.push(currentSelection)
 }
 
+String.prototype.capitalize = function() {
+   	return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+
 function calc() {
 
+	
 	var selection = {}
 
 	// duration
@@ -85,11 +91,11 @@ function calc() {
 			p_device = 3
 			break;
 		case 'pc':
-			p_device = 58
+			p_device = 115
 			break;
 		default:
 			// default is laptop
-			p_device = 28
+			p_device = 32
 	}
 
 	// service type
@@ -98,76 +104,118 @@ function calc() {
 	selection['service'] = 'W'
 	
 	// default for text
-	var dataVolume = 1800000
+	var dataVolume = 8000000
 
 	// change page every minute
 	pageLoads = durationMins
 	if(serviceType == 'video') {
 		// average for video
-		dataVolume = durationSecs * 450000
+		dataVolume = durationSecs * 1100000
 		// when watching video =- only one page
 		pageLoads = 1
 		p_device = p_device * 1.15	
 		selection['service'] = 'V'
 	}
+
 	// power access network
 	var connectionType = document.getElementById('connection_selected').value
 	connectionType = connectionType.trim()
 	selection['connection'] = 'W'
+	var e_origin_per_request = 306 
+	var e_origin = e_origin_per_request * pageLoads
+	var e_3rdp = 6.9E-006 * dataVolume * pageLoads
+	var e_serv = e_3rdp + e_origin
+	var e_network = 5.8616855E-6 * dataVolume * pageLoads
+
 	var e_acc_net = 0
 	switch(connectionType) {
 		case '3G mobile':
-			e_acc_net = dataVolume * 1.8144144197598379e-05 * pageLoads
+			e_acc_net = dataVolume * 4.55e-05 * pageLoads
 			selection['connection'] = 'M'
 			break;
 		default:
-			// default is dsl
+			// default is wifi
 			e_acc_net = 10 * durationSecs
 	}
-	var e_origin = 102 * pageLoads
-	var e_3rdp = 5.3490346705157022e-06 * dataVolume * pageLoads
-	var e_serv = e_3rdp + e_origin
-	var e_network = 5.8616855E-6 * dataVolume * pageLoads
+	
 	var e_user = p_device * durationSecs
 	var e_total_joule = e_serv + e_network + e_acc_net + e_user
+
 	// convert to watthour
 	e_total = e_total_joule / 3600
 	e_total = Math.round(e_total * 100) / 100
+
 	// update the page to show the results
 	document.getElementById('bignr').innerHTML = "<h1><big>" + e_total + "</big><br/> watt hours</h1>"
-	// create percentages
+
+	// calc percentages
 	var p_user = (e_user * 100) / e_total_joule
 	var p_network = (e_network * 100) / e_total_joule
 	var p_serv = (e_serv * 100) / e_total_joule
 	var p_acc_net = (e_acc_net * 100) / e_total_joule
+
+	switch(connectionType) {
+		case '3G mobile':
+			var e_internet = e_network + e_serv + e_acc_net
+			var p_internet = (e_internet * 100) / e_total_joule
+			break;
+		default:
+			var e_internet = e_network + e_serv
+			var p_internet = (e_internet * 100) / e_total_joule		
+	}	
+		
+
 	// create circles
-	var max_size = 8000
-	var circle_user = Math.round(Math.sqrt(max_size * (e_user / 3600) / 3.1416))
-	margin = (100 - circle_user) / 2
-	margintop = (100 - circle_user) / 3
-	document.getElementById('circle_device').innerHTML = '<div style="width:' + circle_user + 'px; height:' + circle_user + 'px;margin-left:' + margin + 'px;margin-top:' + margintop + 'px; background:#C7E2EC; -moz-border-radius: 150px; -webkit-border-radius:150px;"></div>'
-	document.getElementById('text_device').innerHTML = "<h2>" + deviceType + "<br/><br/></h2><p>" + Math.round(e_user / 36) / 100 + " Wh</p><p> " + Math.round(p_user) + " &#37;</p>"
+	var max_size = 8000	
+	switch(connectionType) {
+		case '3G mobile':
+	
+			var circle_user = Math.round(Math.sqrt(max_size * (e_user / 3600) / 3.1416))
+			margin = (100 - circle_user) / 2
+			margintop = (100 - circle_user) / 3
+			document.getElementById('circle_device').innerHTML = '<div style="width:' + circle_user + 'px; height:' + circle_user + 'px;margin-left:' + margin + 'px;margin-top:' + margintop + 'px; background:#C7E2EC; -moz-border-radius: 300px; -webkit-border-radius:300px;"></div>'
+			document.getElementById('text_device').innerHTML = "<h2>" + deviceType.capitalize() + "<br/><br/></h2><p>" + Math.round(e_user / 36) / 100 + " Wh</p><p> " + Math.round(p_user) + " &#37;</p>"
+		
+			var circle_internet = Math.round(Math.sqrt(max_size * (e_internet / 3600) / 3.1416))
+			margin = (100 - circle_internet) / 2
+			margintop = (100 - circle_internet) / 3
+			document.getElementById('circle_infra').innerHTML = '<div style="width:' + circle_internet + 'px; height:' + circle_internet + 'px; margin-left:' + margin + 'px;margin-top:' + margintop + 'px; background:#51A1B7; -moz-border-radius: 300x; -webkit-border-radius:300px;"></div>'
+			document.getElementById('text_infra').innerHTML = "<h2>Internet   <br/><br/></h2><p>" + Math.round(e_internet / 36) / 100 + " Wh</p><p> " + Math.round(p_internet) + " &#37;</p>"
 
-	var circle_network = Math.round(Math.sqrt(max_size * (e_network / 3600) / 3.1416))
-	margin = (100 - circle_network) / 2
-	margintop = (100 - circle_network) / 3
-	document.getElementById('circle_infra').innerHTML = '<div style="width:' + circle_network + 'px; height:' + circle_network + 'px; margin-left:' + margin + 'px;margin-top:' + margintop + 'px; background:#51A1B7; -moz-border-radius: 80px; -webkit-border-radius:80px;"></div>'
-	document.getElementById('text_infra').innerHTML = "<h2>Internet<br/><br/></h2></h2><p>" + Math.round(e_network / 36) / 100 + " Wh</p><p> " + Math.round(p_network) + " &#37;</p>"
+			document.getElementById('circle_access').innerHTML = '<div> </div>'
+			document.getElementById('text_access').innerHTML = "<h2>Home Wi-Fi<br/><br/></br></h2>"
 
-	var circle_serv = Math.round(Math.sqrt(max_size * (e_serv / 3600) / 3.1416))
-	margin = (100 - circle_serv) / 2
-	margintop = (100 - circle_serv) / 3
-	document.getElementById('circle_server').innerHTML = '<div style="width:' + circle_serv + 'px; height:' + circle_serv + 'px;margin-left:' + margin + 'px;margin-top:' + margintop + 'px; background:#4998CE; -moz-border-radius: 80px; -webkit-border-radius:80px;"></div>'
-	document.getElementById('text_server').innerHTML = "<h2>servers<br/><br/></h2><p>" + Math.round(e_serv / 36) / 100 + " Wh</p><p> " + Math.round(p_serv) + " &#37;</p>"
 
-	var circle_acc_net = Math.round(Math.sqrt(max_size * (e_acc_net / 3600) / 3.1416))
-	margin = (100 - circle_acc_net) / 2
-	margintop = (100 - circle_acc_net) / 3
+			break;
+		default:
+		
+		var circle_user = Math.round(Math.sqrt(max_size * (e_user / 3600) / 3.1416))
+		margin = (100 - circle_user) / 2
+		margintop = (100 - circle_user) / 3
+		document.getElementById('circle_device').innerHTML = '<div style="width:' + circle_user + 'px; height:' + circle_user + 'px;margin-left:' + margin + 'px;margin-top:' + margintop + 'px; background:#C7E2EC; -moz-border-radius: 300px; -webkit-border-radius:300px;"></div>'
+		document.getElementById('text_device').innerHTML = "<h2>" + deviceType.capitalize() + "<br/><br/></h2><p>" + Math.round(e_user / 36) / 100 + " Wh</p><p> " + Math.round(p_user) + " &#37;</p>"
+		
+		var circle_acc_net = Math.round(Math.sqrt(max_size * (e_acc_net / 3600) / 3.1416))
+		margin = (100 - circle_acc_net) / 2
+		margintop = (100 - circle_acc_net) / 3
+		
+		document.getElementById('circle_access').innerHTML = '<div style="width:' + circle_acc_net + 'px; height:' + circle_acc_net + 'px;margin-left:' + margin + 'px;margin-top:' + margintop + 'px; background:#A3C3D8; -moz-border-radius: 300px; -webkit-border-radius:300px;"></div>'
+		document.getElementById('text_access').innerHTML = "<h2>Home Wi-Fi<br/><br/></h2><p>" + Math.round(e_acc_net / 36) / 100 + " Wh</p><p> " + Math.round(p_acc_net) + " &#37;</p>"
+
+		var circle_internet = Math.round(Math.sqrt(max_size * (e_internet / 3600) / 3.1416))
+		margin = (100 - circle_internet) / 2
+		margintop = (100 - circle_internet) / 3
+		document.getElementById('circle_infra').innerHTML = '<div style="width:' + circle_internet + 'px; height:' + circle_internet + 'px; margin-left:' + margin + 'px;margin-top:' + margintop + 'px; background:#51A1B7; -moz-border-radius: 300px; -webkit-border-radius:300px;"></div>'
+		document.getElementById('text_infra').innerHTML = "<h2>Internet<br/><br/></h2></h2><p>" + Math.round(e_internet / 36) / 100 + " Wh</p><p> " + Math.round(p_internet) + " &#37;</p>"
+	
+		
+	
+	}
 	if(connectionType == "3G mobile"){
 		connectionType = "3G mobile connection"
 	}
-	document.getElementById('circle_access').innerHTML = '<div style="width:' + circle_acc_net + 'px; height:' + circle_acc_net + 'px;margin-left:' + margin + 'px;margin-top:' + margintop + 'px; background:#A3C3D8; -moz-border-radius: 40px; -webkit-border-radius:80px;"></div>'
-	document.getElementById('text_access').innerHTML = "<h2>" + connectionType + "</h2><p>" + Math.round(e_acc_net / 36) / 100 + " Wh</p><p> " + Math.round(p_acc_net) + " &#37;</p>"
+
+
 
 	//drawChart(e_serv, e_network, e_acc_net, e_user)
 	calcLightBulbsAndCarbon(e_total_joule, durationSecs)
